c05754e1f70462d969a4398cdb5b79df31d263a3
jan-lindner00 <mail.jnvpn@gmail.com>
2026-07-30 20:42:15 +0200

    fixed bug where you couldn't select play as O

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,9 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router"
 import AppContextProvider from "./context/AppContextProvider.tsx"
 import {router} from "./router.tsx"
-import { StrictMode } from "react";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
-    <StrictMode>
         <AppContextProvider>
             <RouterProvider router={router} />
         </AppContextProvider>
-    </StrictMode>
 )
